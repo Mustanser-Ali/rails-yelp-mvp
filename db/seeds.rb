7a625309e7e35d6f3b92c297ae92f341e329407f
mustanser ali <razaali199913@outlook.com>
2023-05-24 14:09:13 +0200

    ugly but working yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'intiallizing seeding.....'
+5.times do
+  new_restaurants = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.city, phone_number: Faker::PhoneNumber.cell_phone, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+  new_restaurants.save!
+end
